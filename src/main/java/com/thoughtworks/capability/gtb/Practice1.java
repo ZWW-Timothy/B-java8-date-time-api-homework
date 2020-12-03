9eb8398d7fba37d482689d0edc21de39ec053446
ZW <zhaowuwei2020@foxmail.com>
2020-12-03 14:28:09 +0800

    [ZWW]: Finish practice 1.

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +12,11 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+
+    LocalDate nextLaborDay = LocalDate.of(date.getYear(), 5,1);
+    if (date.isAfter(nextLaborDay)) {
+      nextLaborDay = nextLaborDay.plusYears(1);
+    }
+    return date.until(nextLaborDay, ChronoUnit.DAYS);
   }
 }
